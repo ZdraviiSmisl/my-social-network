a5fb205adaa72afbb7524a102691a1552a061726
evgeniy <gormoshka@gmail.com>
2023-04-23 17:54:30 +0300

    Moving to the root of the index.js file

---
+++ index.js
@@ -0,0 +1,31 @@
+import Heading from "./src/components/Heading";
+import styles from "../../styles/Home.module.scss";
+import Head from "next/head";
+import custom from "../../styles/Button.module.scss";
+import { Button } from "./src/components/Button";
+import { setAuthUser } from "./src/store/action-creators-posts";
+import { useDispatch, useSelector } from "react-redux";
+
+const Home = () => {
+  const { authUser } = useSelector((state) => state.authReducer);
+  const dispatch = useDispatch();
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Home</title>
+      </Head>
+      <Heading text="Home page" />
+      {authUser && (
+        <Button
+          onClick={() => dispatch(setAuthUser(false))}
+          buttonStyle={custom.btnPrimaryOutline}
+          btn={custom.btn}
+          type="button"
+          text="Log Out"
+        />
+      )}
+    </div>
+  );
+};
+
+export default Home;
